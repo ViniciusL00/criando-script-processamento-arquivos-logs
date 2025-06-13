2d8c66d9181dfed7bf341834b7aa06041d6b4212
Vinicius Leme <vinicius.leme1312@gmail.com>
2025-06-13 14:26:03 -0300

    Refatorando o Script

--- monitorando-logs.sh
+++ monitorando-logs.sh
@@ -23,10 +23,10 @@ find $LOG_DIR -name "*.log" -print0 | while IFS= read -r -d '' arquivo; do
 
     nome_arquivo=$(basename "${arquivo}.unico")
 
-    echo "Arquivo: $nome_arquivo" >> log_stats.txt
-    echo "Número de linhas: $num_linhas" >> log_stats.txt
-    echo "Número de palavras: $num_palavras" >> log_stats.txt
-    echo "---------------------" >> log_stats.txt
+    echo "Arquivo: $nome_arquivo" >> "${ARQUIVO_DIR}/log_stats_$(date +%F).txt"
+    echo "Número de linhas: $num_linhas" >> "${ARQUIVO_DIR}/log_stats_$(date +%F).txt"
+    echo "Número de palavras: $num_palavras" >> "${ARQUIVO_DIR}/log_stats_$(date +%F).txt"
+    echo "---------------------" >> "${ARQUIVO_DIR}/log_stats_$(date +%F).txt"
 
     cat "${arquivo}.unico" >> "${ARQUIVO_DIR}/logs_combinados_$(date +%F).log"
 done
